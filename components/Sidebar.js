9d6a1b24aa2973bd55a17776fda66eaa690bab04
Mai Trung Hieu <maitrunghieu199hp@gmail.com>
2022-02-02 00:29:01 +0700

    update

---
+++ components/Sidebar.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import React from 'react';
+
+function Sidebar() {
+  return <section className=''>
+    <Image
+      src="https://rb.gy/xkacau"
+      width={56}
+      height={56}
+      objectFit="contain"
+    />
+    <div>
+
+    </div>
+  </section>
+}
+
+export default Sidebar;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Dashboard from '../components/Dashboard'
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Dashboard></Dashboard>
+    </div>
+  )
+}
